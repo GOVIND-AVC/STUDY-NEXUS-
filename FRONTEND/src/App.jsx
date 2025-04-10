26ac84ee15000eaad25d9a6ede2a638b6a666cbc
GOVIND AVC <eemail2govindavc@gmail.com>
2025-04-10 09:11:36 +0530

    Added Login page frontend and linked the Login button to home

--- FRONTEND/src/App.jsx
+++ FRONTEND/src/App.jsx
@@ -2,16 +2,24 @@ import React from 'react'
 import { BrowserRouter as Router,Routes,Route } from 'react-router-dom'
 import Login from './pages/Login'
 import Signup from './pages/Signup'
+import { Toaster } from 'react-hot-toast'
+import { Home } from 'lucide-react'
+import HOME from './pages/HOME.JSX'
 
 
 const App = () => {
   return (
+    <>
+    <Toaster position='top-center' reverseOrder={false} />
     <Router>
       <Routes>
         <Route path='/signup' element={<Signup/>}/>
         <Route path='/login' element={<Login/>}/>
+        <Route path='/home' element={<HOME/>}/>
       </Routes>
     </Router>
+    </>
+    
 
 
   )
